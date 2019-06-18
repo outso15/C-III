59095e12f2ece89090c5226e0446af6bb6ea2072
outso15 <42581372+outso15@users.noreply.github.com>
2019-06-18 17:12:15 -0500

    Update parser_rules.py

--- lexer_parser 2.0/parser_rules.py
+++ lexer_parser 2.0/parser_rules.py
@@ -2,24 +2,24 @@ from lexer_rules import tokens
 from expressions import *
 
 def p_expression_plus(subexpr):
-    'expression : expression term PLUS'
+    'expression : expression expression PLUS'
     subexpr[0] = subexpr[1] + subexpr[2]
+    
+def p_expression_minus(subexpr):
+    'expression : expression expression MINUS'
+    subexpr[0] = subexpr[1] - subexpr[2]
+    
+def p_expression_times(subexpr):
+    'expression : expression expression TIMES'
+    subexpr[0] = subexpr[1] * subexpr[2]
+    
+def p_expression_div(subexpr):
+    'expression : expression expression DIV'
+    subexpr[0] = subexpr[1] / subexpr[2]
 
 def p_expression_term(subexpr):
     'expression : term'
     subexpr[0] = subexpr[1]
-
-def p_term_times(subexpr):
-    'term : term factor TIMES'
-    subexpr[0] = subexpr[1] * subexpr[2]
-
-def p_expression_minus(subexpr):
-    'expression : expression term MINUS'
-    subexpr[0] = subexpr[1] - subexpr[2]
-
-def p_term_div(subexpr):
-    'term : term factor DIV'
-    subexpr[0] = subexpr[1] / subexpr[2]
 
 def p_term_factor(subexpr):
     'term : factor'
@@ -28,11 +28,6 @@ def p_term_factor(subexpr):
 def p_factor_num(subexpr):
     'factor : NUMBER'
     subexpr[0] = subexpr[1]
-    
-def p_factor_expr(subexpr):
-    'factor : expression'
-    subexpr[0] = subexpr[1]
 
 def p_error(subexpr):
-    print(subexpr)
     raise Exception("Syntax error.")
