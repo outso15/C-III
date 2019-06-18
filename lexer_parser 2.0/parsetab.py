a561b4409c4dea5fc6386d1328ef7e45a56d9e97
outso15 <42581372+outso15@users.noreply.github.com>
2019-06-18 17:47:32 -0500

    Update parsetab.py

--- lexer_parser 2.0/parsetab.py
+++ lexer_parser 2.0/parsetab.py
@@ -5,9 +5,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'NUMBER PLUS TIMES MINUS DIVIDE expression : expression term PLUSexpression : termterm : term factor TIMESterm : term factor DIVIDEterm : factorfactor : NUMBER'
+_lr_signature = 'NUMBER PLUS TIMES MINUS DIVIDEexpression : expression expression PLUSexpression : expression expression MINUSexpression : expression expression TIMESexpression : expression expression DIVIDEexpression : termterm : factorfactor : NUMBER'
     
-_lr_action_items = {'DIV':([2,5,],[-6,8,]),'TIMES':([2,5,],[-6,7,]),'PLUS':([2,3,6,7,8,],[-6,-5,9,-3,-4,]),'NUMBER':([0,1,2,3,4,6,7,8,9,],[2,2,-6,-5,2,2,-3,-4,-1,]),'$end':([1,2,3,4,7,8,9,],[-2,-6,-5,0,-3,-4,-1,]),}
+_lr_action_items = {'DIVIDE':([1,2,3,5,6,7,8,9,],[-5,-7,-6,6,-4,-3,-1,-2,]),'NUMBER':([0,1,2,3,4,5,6,7,8,9,],[2,-5,-7,-6,2,2,-4,-3,-1,-2,]),'TIMES':([1,2,3,5,6,7,8,9,],[-5,-7,-6,7,-4,-3,-1,-2,]),'PLUS':([1,2,3,5,6,7,8,9,],[-5,-7,-6,8,-4,-3,-1,-2,]),'MINUS':([1,2,3,5,6,7,8,9,],[-5,-7,-6,9,-4,-3,-1,-2,]),'$end':([1,2,3,4,6,7,8,9,],[-5,-7,-6,0,-4,-3,-1,-2,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -16,7 +16,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'term':([0,4,],[1,6,]),'expression':([0,],[4,]),'factor':([0,1,4,6,],[3,5,3,5,]),}
+_lr_goto_items = {'term':([0,4,5,],[1,1,1,]),'expression':([0,4,5,],[4,5,5,]),'factor':([0,4,5,],[3,3,3,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -26,10 +26,11 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> expression","S'",1,None,None,None),
-  ('expression -> expression term PLUS','expression',3,'p_expression_plus','parser_rules.py',5),
-  ('expression -> term','expression',1,'p_expression_term','parser_rules.py',9),
-  ('term -> term factor TIMES','term',3,'p_term_times','parser_rules.py',13),
-  ('term -> term factor DIV','term',3,'p_term_div','parser_rules.py',22),
-  ('term -> factor','term',1,'p_term_factor','parser_rules.py',26),
-  ('factor -> NUMBER','factor',1,'p_factor_num','parser_rules.py',30),
+  ('expression -> expression expression PLUS','expression',3,'p_expression_plus','parser_rules.py',5),
+  ('expression -> expression expression MINUS','expression',3,'p_expression_minus','parser_rules.py',9),
+  ('expression -> expression expression TIMES','expression',3,'p_expression_times','parser_rules.py',13),
+  ('expression -> expression expression DIVIDE','expression',3,'p_expression_divide','parser_rules.py',17),
+  ('expression -> term','expression',1,'p_expression_term','parser_rules.py',21),
+  ('term -> factor','term',1,'p_term_factor','parser_rules.py',25),
+  ('factor -> NUMBER','factor',1,'p_factor_num','parser_rules.py',29),
 ]
