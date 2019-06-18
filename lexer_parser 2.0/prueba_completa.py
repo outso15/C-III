97b934475e8cdbb8c7a9159dc659f491e35b7216
outso15 <42581372+outso15@users.noreply.github.com>
2019-06-18 17:47:58 -0500

    Update prueba_completa.py

--- lexer_parser 2.0/prueba_completa.py
+++ lexer_parser 2.0/prueba_completa.py
@@ -1,16 +1,34 @@
 import lexer_rules
 import parser_rules
-import getFile
 
 from ply.lex import lex
 from ply.yacc import yacc
 
-text = getFile.getFiles()
-lexer = lex(module=lexer_rules)
-parser = yacc(module=parser_rules)
-expression = parser.parse(text, lexer)
-print expression
-"""
-result = expression.evaluate()
-print result
-"""
+class Prueba_Completa:
+    def abrir_archivo(self):
+        
+        expresiones = open("expresion.txt")
+        linea = [" "]
+        impresion = ''
+        while linea != '':
+           
+            linea = expresiones.readline().split(' ')
+            if (linea == ['']):
+                expresiones.close()
+                break
+           
+            lexer = lex(module=lexer_rules)
+            parser = yacc(module=parser_rules)
+            expression = parser.parse(' '.join(map(str, linea[:-1])).strip('[]'), lexer)
+            
+            impresion += str(expression)
+        return impresion
+
+    def escribir_archivo(self,resultado):
+        busquedas = open("resultado.txt", "w")
+        busquedas.write(resultado)
+        busquedas.close()
+
+prueba = Prueba_Completa()
+salida = prueba.abrir_archivo()
+prueba.escribir_archivo(salida)
